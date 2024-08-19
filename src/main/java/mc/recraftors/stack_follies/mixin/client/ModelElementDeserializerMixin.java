8ead0bb9f65c0326a594affe814e798f6a19b40c
Nerjal Nosk <nosk.nerjal@gmx.com>
2024-08-19 23:41:29 +0200

    Simplified model element name deserializing injection

--- src/main/java/mc/recraftors/stack_follies/mixin/client/ModelElementDeserializerMixin.java
+++ src/main/java/mc/recraftors/stack_follies/mixin/client/ModelElementDeserializerMixin.java
@@ -17,25 +17,21 @@ import java.lang.reflect.Type;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ModelElement.Deserializer.class)
-public class ModelElementDeserializerMixin {
+public abstract class ModelElementDeserializerMixin {
 
     @Inject(
             method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lnet/minecraft/client/render/model/json/ModelElement;",
-            at = @At("RETURN")
+            at = @At("TAIL")
     )
     private void sf_deserializeReturnNamed(JsonElement element, Type type, JsonDeserializationContext context,
                                            CallbackInfoReturnable<ModelElement> cir) {
         JsonObject object = element.getAsJsonObject();
-        String s = null;
         if (object.has(StackFolliesClient.MODEL_ELEM_NAME_KEY)) {
             JsonElement e = object.get(StackFolliesClient.MODEL_ELEM_NAME_KEY);
             if (e.isJsonPrimitive() && e.getAsJsonPrimitive().isString()) {
-                s = e.getAsString();
+                String s = e.getAsString();
+                ((NamedElementAccessor)cir.getReturnValue()).sf_setElemName(s);
             }
         }
-        if (s == null) {
-            return;
-        }
-        ((NamedElementAccessor)cir.getReturnValue()).sf_setElemName(s);
     }
 }
